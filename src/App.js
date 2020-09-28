6035a7f88fd61d4e0553fd27e365d57de1d59076
Isaac <isaac1180@aol.com>
2020-09-28 11:15:27 -0400

    new repo for project

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let num = "getting file"; 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Show {num}
         </p>
         <a
           className="App-link"
