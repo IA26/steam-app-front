4db2586929bbd26fe7aa6a7374525de9431ad8d3
Isaac <isaac1180@aol.com>
2020-09-29 11:49:10 -0400

    fixing fetch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import './App.css';
 import {Switch, Route, withRouter, Redirect} from 'react-router-dom'
 
 class App extends React.Component {
-  
+
+  componentDidMount() {
+    fetch(`https://partner-api.yelp.com/v1/business_info/`)
+    .then(r => r.json())
+    .then((applist) => 
+      this.setState({
+        applist
+      })
+    )}
+
+  state = {
+    applist : {}
+  }
+
+
   render() {
+    console.log(this.applist)
     return (
       <>
-        
+        <div className="App">
+            Show me {this.applist}
+        </div> 
       </>
     )
   }
