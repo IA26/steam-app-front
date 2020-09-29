d55075433565c3951061458c6e07ab366f1b547c
Isaac <isaac1180@aol.com>
2020-09-29 10:48:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Show {num} - test
+          Show {num} 
         </p>
         <a
           className="App-link"
