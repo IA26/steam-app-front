d4ce1b59427c56ad0d3b7ddf5474fadf91cfcfff
Isaac <isaac1180@aol.com>
2020-09-29 09:56:18 -0400

    testing for new repo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Show {num}
+          Show {num} - test
         </p>
         <a
           className="App-link"
